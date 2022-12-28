4db8dd12bf406c01aac7b87e7d3c574019a879b8
Rafael Velasquez <rafa_el171@yahoo.com>
2022-12-28 17:13:02 -0400

    install: installation and configuration jwt-auth

---
+++ tests/Feature/Auth/RegisterUserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function a_user_can_be_register()
+    {
+        //
+    }
+}
